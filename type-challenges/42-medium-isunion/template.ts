f6560a7a4d45c0291a5f49d967d9c9f0c455d241
lvhaizhou <417703682@qq.com>
2022-04-20 08:55:26 +0800

    feat:replacekeys

--- type-challenges/42-medium-isunion/template.ts
+++ type-challenges/42-medium-isunion/template.ts
@@ -13,6 +13,6 @@ type IsUnion<T, U = T> = T extends U
 
 // 在条件类型中，联合将是分配的，看看下面的例子：
 type Test<T, T2 = T> = T extends T2 ? {t: T, t2: T2} : true
-type a = Test<string|number|void> // {t: string, t2: string | number} | {t: number, t2: string | number}
+type a = Test<string|number> // {t: string, t2: string | number} | {t: number, t2: string | number}
 type b = Test<string> // {t: string, t2: string}
 // 当分布发生时，T和T2之间存在差异。现在我们可以通过将T2赋回T来判断T是否为并集类型。这次我们用方括号来避免分配。
