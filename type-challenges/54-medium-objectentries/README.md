f4b0c41ab4c073ab524c04e14bf02c4d909db193
lvhaizhou <417703682@qq.com>
2022-04-24 11:28:01 +0800

    feat:objectentries

---
+++ type-challenges/54-medium-objectentries/README.md
@@ -0,0 +1,12 @@
+实现 `TS` 类型版的 `Object.entries`
+
+For example 
+
+```typescript
+interface Model {
+  name: string;
+  age: number;
+  locations: string[] | null;
+}
+type modelEntries = ObjectEntries<Model> // ['name', string] | ['age', number] | ['locations', string[] | null];
+```
---
+++ type-challenges/54-medium-objectentries/template.ts
@@ -0,0 +1,21 @@
+// type ObjectEntries<T> = any
+
+// type ObjectEntries<T> = {
+//   [P in keyof T]: [P, T[P]]
+// }[keyof T]
+
+type ObjectEntries<T> = keyof T extends infer P 
+  ? P extends keyof T 
+    ? [P, T[P] extends infer R | undefined 
+      ? R 
+      : T[P]] 
+    : never 
+  : never;
+
+
+interface Model {
+  name?: string
+  age: number
+  locations: string[] | null
+}
+type p1 = ObjectEntries<Model>
